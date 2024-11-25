d7c7e9be58fccd2700d733fd3159fd0596a6d469
FELIPE FERNANDES <felipe.ff58@gmail.com>
2024-11-25 19:16:59 -0300

    Update main.c 10

--- main.c
+++ main.c
@@ -1,45 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main(){
-
-    int linha, coluna, **matriz;
     
-    scanf("%d %d", &linha, &coluna);
-    if(linha == 0 || coluna == 0){
-        printf("[vazio]");
-        return 0;
-    }
-    matriz = (int **)malloc(linha * sizeof(int));
-        if(matriz == NULL){
-        printf("Memória não alocada para as linhas.\n");
-        return 1;
-    }
-    for (int i = 0; i < linha; i++){
-    matriz[i] = (int *)malloc(coluna * sizeof(int));
-        if(matriz[i] == NULL){
-            printf("Memória não alocada para as colunas.\n");
-            return 1;
-        }
-    }
-        
-    for(int i = 0; i < linha; i++){
-        for (int j = 0; j < coluna; j++){
-            scanf("%d", &matriz[i][j]);
-        }
-    }
-    
-    for(int i = 0; i < linha; i++){
-        for(int j = 0; j < coluna; j++){
-            printf("%d ", matriz[i][j]);
-        }
-        printf("\n");
-    }
+   int **mat; 
+   int linhas,colunas;
 
-        for (int i = 0; i < linha; i++){
-            free(matriz[i]);
-        }
-
-        free(matriz);
+   scanf("%d %d",&linhas,&colunas);
     
+   if(linhas == 0 ){
+     printf("[matriz vazia]");
+   }else {
+  
+   mat = malloc(linhas*sizeof(int*));
+
+   for(int i=0;i<linhas;i++){
+     mat[i] = (int*)malloc(colunas * sizeof(int));
+   }
+   for(int i=0;i<linhas;i++){
+      for(int j=0;j<colunas;j++){
+           scanf("%d",&mat[i][j]);
+      }
+   }
+   for(int i=0;i<linhas;i++){
+      for(int j=0;j<colunas;j++){
+           printf("%d", mat[i][j]);
+           if(j != colunas-1){
+            printf(" ");
+           }
+           
+      }
+       printf("\n");
+   }
+   for(int i=0;i<linhas;i++){
+    free(mat[i]);
+   }
+    free(mat);
+   }   
     return 0;
 }
